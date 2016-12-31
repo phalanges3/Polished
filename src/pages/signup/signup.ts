266c806f69721f46e2cd6feaa595e778ecb6e7ba
Joshua Pace <joshpace@gmail.com>
2016-12-31 13:52:50 -0800

    [add] progress on client-side routing

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http'
 
 /*
   Generated class for the Signup page.
@@ -13,10 +14,23 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignUpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+  addSignup() {
+    this.http
+      .get('http://localhost:3000/api/user/signup')
+      .map((res) => {
+        res.json()
+      })
+      .subscribe((data) => {
+
+      })
+
+
+
   }
 
 }
