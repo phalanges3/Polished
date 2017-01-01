94009063bd8d167d9b3bfbe74ea45609dc0f595b
Joshua Pace <joshpace@gmail.com>
2016-12-31 23:52:37 -0800

    [add] login progress

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,25 +9,13 @@ import { UsernameValidator } from  '../../validators/username'
 })
 export class HomePage {
  
-signupForm: FormGroup;
  
     submitAttempt: boolean = false;
  
     constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
-       this.signupForm = formBuilder.group({
-        firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-        lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-        email: [''],
-        username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')]), UsernameValidator.checkUsername],
-        privacy: ['', Validators.required],
-        bio: ['']
-      })
+       
     }
  
-    save(){
-      this.submitAttempt = true;
-      console.log("success!")
-      console.log(this.signupForm.value);
-    }
+    
  
 }
