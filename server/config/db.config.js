2f5ae48b46c71ef2afccdaa22f976d939d7627f1
Joshua Pace <joshpace@gmail.com>
2017-01-10 19:16:14 -0800

    commented creds

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 // Please create a credential.json file with your username and password for your cloud DB.
 // Refer to sampleCredentials.json
-const creds = require('./credentials')
+//const creds = require('./credentials')
 
 // Database connection
 const DBConnection = new Sequelize('postgres://pmbwfnib:Mkxsvk0pwQLPkxsCTf_mSvoO4fy7QwXW@elmer.db.elephantsql.com:5432/pmbwfnib')
