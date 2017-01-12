a2ee4d2e3a8a7b3ab793a5a5032934793ca289ad
Joshua Pace <joshpace@gmail.com>
2017-01-11 16:34:57 -0800

    cors add

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,7 +7,10 @@ module.exports = (app, express) => {
   app.use(morgan('dev'))
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended: true}))
-  app.use(cors())
+  app.use(cors({
+    origin: true,
+    credentials: true
+  }))
   app.use(express.static(path.join(__dirname, '../../www')))
 }
 
