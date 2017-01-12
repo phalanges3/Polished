9684f4fb0f603f5cc3fef6a6ed2d88c714d59c08
Joshua Pace <joshpace@gmail.com>
2017-01-12 14:33:23 -0800

    add https

--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
 require('./config/db.config.js')
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const http = require('http')
+const https = require('https')
+const HTTP_PORT = process.env.PORT || 3000
+const HTTPS_PORT = 443
 const routes = require('./api/routes.main')
+const fs = require('fs')
 
 // Middleware
 require('./config/middleware.js')(app, express)
+
 // Routes
 app.use('/api', routes)
 
-app.listen(PORT, () => {
-  console.log('SERVER listening on port: ', PORT)
+// HTTPS
+let secureServer = https.createServer({
+    key: fs.readFileSync('./server/config/private.key'),
+    cert: fs.readFileSync('./server/config/certificate.pem')
+  }, app)
+  .listen(HTTPS_PORT,  () => {
+    console.log('Secure Server listening on port: ' + HTTPS_PORT)
 })
+
+// HTTP
+
+var insecureServer = http.createServer(app).listen(HTTP_PORT, () => {
+  console.log('Insecure Server listening on port: ' + HTTP_PORT)
+})
+
+// app.listen(PORT, () => {
+//   console.log('SERVER listening on port: ', PORT)
+// })
 
 module.exports = app
